9027c68bd95a40dc6a25b395a6f3c9fa33d0ad1a
Katsuhiro Ueno <uenobk@gmail.com>
2023-08-17 14:20:39 +0900

    chore(release): update VERSION at each release

--- release.config.js
+++ release.config.js
@@ -4,7 +4,18 @@ export default {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
-    ['@semantic-release/git', { assets: ['package.json'] }],
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: String.raw`sed -i~ 's/\(VERSION *= *\)'\''.*'\''/\1'\''${nextRelease.version}'\''/' *.js`
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', '*.js']
+      }
+    ],
     '@semantic-release/github'
   ]
 }
